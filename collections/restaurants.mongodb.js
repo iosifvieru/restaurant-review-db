2149006114c8e67de16304a03d2d066bfbfe995a
Iosif Vieru <iosifvieru440@gmail.com>
2025-04-30 18:30:18 +0300

    modificari insert many

--- collections/restaurants.mongodb.js
+++ collections/restaurants.mongodb.js
@@ -30,14 +30,15 @@ Structura tabelei "restaurants" si exemplu de document.
         denumire_produs: str,
         pret: double,
         imagini: list[str]
+        ...
     },
     ...
 ]
++ nr_telefon: str
 */
 db.createCollection("restaurants");
 
 /* inserare documente */
-
 db.restaurants.insertMany([
     {
         id: 1,
@@ -56,8 +57,8 @@ db.restaurants.insertMany([
             "miercuri": [{open: "08:30", close: "20:30"}],
             "joi": [{open: "08:30", close: "20:30"}],
             "vineri": [{open: "08:30", close: "20:30"}],
-            "sambata": [{ open: "08:30", close: "20:30" }],
-            "duminica": [{ open: "08:30", close: "20:30" }]
+            "sambata": [{open: "08:30", close: "20:30"}],
+            "duminica": [{open: "08:30", close: "20:30"}]
         },
         meniu: [
             {
@@ -84,6 +85,88 @@ db.restaurants.insertMany([
                     "./images/sutadegrame/panini-picante-3.png",
                 ]
             }
+        ],
+    },
+    {    
+        id: 2,
+        nume: "JUD Shaorma Arabeasca",
+        tip_bucatarie: [
+            "specific arabesc",
+        ],
+        adresa: {
+            strada: "Strada Silvestru 1",
+            oras: "Iași",
+            cod_postal: "700090"
+        },
+        program: {
+            "luni": [{open: "9:30", close: "22:30"}],
+            "marti": [{open: "9:30", close: "22:30"}],
+            "miercuri": [{open: "9:30", close: "22:30"}],
+            "joi": [{open: "9:30", close: "22:30"}],
+            "vineri": [{open: "9:30", close: "22:30"}],
+            "sambata": [{open: "10:00", close: "22:30"}],
+            "duminica": [{open: "10:00", close: "22:30"}]
+        },
+        meniu: [
+            {
+                denumire_produs: "Shaorma arabeasca",
+                pret: 30,
+                imagini: [
+                    "./images/jud/sa-1.png",
+                    "./images/jud/sa-2.png"
+                ],
+            },
+            {
+                denumire_produs: "Shaorma mixt",
+                pret: 31,
+                imagini: [
+                    "./images/jud/samixt-1.png",
+                ]
+            },
+            {
+                denumire_produs: "Pepsi cola",
+                pret: 9,
+                informatii_suplimentare: "Garantie SGR"
+            }
         ]
+    },
+    {   
+        id: 3,
+        nume: "McDonald's",
+        tip_bucatarie: [
+            "fast-food",
+            "specific american",
+        ],
+        adresa: {
+            strada: "Piața Gării nr. 4",
+            oras: "Iași",
+            cod_postal: "700090"
+        },
+        program: {
+            "luni": [{open: "7:00", close: "00:00"}],
+            "marti": [{open: "7:00", close: "00:00"}],
+            "miercuri": [{open: "7:00", close: "00:00"}],
+            "joi": [{open: "7:00", close: "00:00"}],
+            "vineri": [{open: "7:00", close: "00:00"}],
+            "sambata": [{open: "7:00", close: "00:00"}],
+            "duminica": [{open: "7:00", close: "00:00"}]
+        },
+        meniu: [
+            {
+                denumire_produs: "Big Tasty",
+                pret: 20.50,
+                imagini: [
+                    "./images/mcdo/big-tasty-1.png",
+                ],
+            },
+            {
+                denumire_produs: "Mc Combo",
+                pret: 9.90,
+                imagini: [
+                    "./images/mcdo/mc-combo-1.png",
+                ]
+            },
+        ],
+        nr_telefon: "0372 701 047"
     }
 ])
--- collections/reviews.mongodb.js
+++ collections/reviews.mongodb.js
@@ -18,4 +18,35 @@ Structura tabelei "reviews" si exemplu de document.
 db.createCollection("reviews");
 
 /* inserare documente */
-
+db.reviews.insertMany([
+    {
+        id_user: 3,
+        id_restaurant: 1,
+        scor: 3.85,
+        comentariu: "Bun calitate pret, decent dpv. al gustului. Clar as recomanda.",
+        data: new Date()
+    },
+    {
+        id_user: 1,
+        id_restaurant: 3,
+        scor: 4.58,
+        comentariu: "Am comandat legendarul Big Tasty, bun ca intotdeauna.",
+        data: new Date(),
+        
+        likes: 100,
+        comments: [
+            {
+                id_user: 3,
+                text: "Si eu, la Gara... super bun."
+            }
+        ]
+    },
+    {
+        id_user: 2,
+        id_restaurant: 3,
+        scor: 3.00,
+        comentariu: "Bun, dar n-am putut consuma cartofii, sunt la dieta Keto.",
+        data: new Date(),
+        likes: 112
+    }
+])
